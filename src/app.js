37deb4e47b69f8b97b32c120c2254fb90e70ead2
Davidrg02 <davrodriguezgu@unal.edu.co>
2024-04-10 20:47:49 -0500

    routes app

--- src/app.js
+++ src/app.js
@@ -4,6 +4,9 @@ const config = require('./config');
 const cors = require('cors');
 
 const municipios = require('./modules/municipio/routes');
+const departamentos = require('./modules/departamento/routes');
+const personas = require('./modules/persona/routes');
+const viviendas = require('./modules/vivienda/routes');
 const errors = require('./network/errors');
 
 const app = express();
@@ -21,6 +24,9 @@ app.set('port', config.app.port);
 
 // Routes
 app.use('/api/municipios', municipios);
+app.use('/api/departamentos', departamentos);
+app.use('/api/personas', personas);
+app.use('/api/viviendas', viviendas);
 
 app.use(errors);
 
