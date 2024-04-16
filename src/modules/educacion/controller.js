e043c465b44329485146972e628f426e894791f9
nihernandezv <nihernandezv@unal.edu.co>
2024-04-15 20:54:05 -0500

    Formato fechas arreglado

--- src/modules/educacion/controller.js
+++ src/modules/educacion/controller.js
@@ -11,8 +11,8 @@ async function all() {
             SELECT 
                 e.idEducacion, 
                 e.institucionNombre,
-                e.fechaInicio,
-                e.fechaFin, 
+                DATE_FORMAT(e.fechaInicio, '%d/%m/%Y') AS fechaInicio,
+                DATE_FORMAT(e.fechaFin, '%d/%m/%Y') AS fechaFin, 
                 ne.nivelEducacion,
                 p.idPersona, 
                 p.personaNombre, 
@@ -35,8 +35,35 @@ async function all() {
 }
 
 
-function one(id) { 
-    return db.one(table, id, idPersonaField);
+async function one(id) { 
+    try {
+        const query = `
+            SELECT 
+                e.idEducacion, 
+                e.institucionNombre,
+                DATE_FORMAT(e.fechaInicio, '%d/%m/%Y') AS fechaInicio,
+                DATE_FORMAT(e.fechaFin, '%d/%m/%Y') AS fechaFin, 
+                ne.nivelEducacion,
+                p.idPersona, 
+                p.personaNombre, 
+                p.personaApellido
+            FROM    
+                EDUCACION e
+            JOIN
+                NIVEL_DE_EDUCACION ne ON e.NIVEL_DE_EDUCACION_idNivelEducacion = ne.idNivelEducacion
+            JOIN 
+                PERSONA p ON e.PERSONA_idPersona = p.idPersona
+            WHERE
+                p.idPersona = ${id}
+        `;
+        
+        // Ejecuta la consulta SQL
+        const result = await db.customQuery(query);
+
+        return result;
+    } catch (error) {
+        throw new Error('Error al obtener personas con estudios: ' + error.message);
+    }
 }
 
 function insert(data) {
